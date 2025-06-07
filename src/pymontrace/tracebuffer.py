e6c87aebdd2329ba8fc11bf106ac68dc28e65c47
cakemanny <goldingd89@gmail.com>
2025-06-07 13:47:11 +0200

    tracebuffer: finish rename from mapbuffer

--- src/pymontrace/tracebuffer.py
+++ src/pymontrace/tracebuffer.py
@@ -11,12 +11,12 @@ DEFAULT_BUFFER_SIZE = (1 << 20)  # 1MiB
 
 
 class TraceBuffer:
-    def __init__(self, _mb) -> None:
-        self._mb = _mb
+    def __init__(self, _tb) -> None:
+        self._tb = _tb
         self._lock = threading.Lock()
 
     def close(self):
-        self._mb = None  # __del__ will clean up
+        self._tb = None  # __del__ will clean up
 
     def __enter__(self):
         return self
@@ -25,18 +25,18 @@ class TraceBuffer:
         self.close()
 
     def read(self) -> bytes:
-        if (mb := self._mb) is None:
+        if (tb := self._tb) is None:
             raise ValueError('I/O operation on closed buffer')
-        return mb.read()
+        return tb.read()
 
     def write(self, data: bytes) -> None:
-        if (mb := self._mb) is None:
+        if (tb := self._tb) is None:
             raise ValueError('I/O operation on closed buffer')
 
         if not self._lock.acquire(timeout=1.0):
             raise TimeoutError('failed to acquire lock on TraceBuffer after 1s')
         try:
-            return mb.write(data)
+            return tb.write(data)
         finally:
             self._lock.release()
 
@@ -47,5 +47,5 @@ def create(filename: str, size: int = DEFAULT_BUFFER_SIZE) -> TraceBuffer:
     fd = os.open(filename, os.O_CREAT | os.O_RDWR)
     os.ftruncate(fd, size)
     with open(fd, 'a+b', buffering=0) as f:  # <- closes the fd
-        mb = _tracebuffer.create(f.fileno())
-    return TraceBuffer(mb)
+        tb = _tracebuffer.create(f.fileno())
+    return TraceBuffer(tb)
