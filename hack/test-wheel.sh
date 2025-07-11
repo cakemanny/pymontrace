476e0e3680528ba3b9ff735b55fb7ae211676e18
cakemanny <goldingd89@gmail.com>
2025-07-11 13:45:35 +0200

    ci: refactor wheel testing into script

---
+++ hack/test-wheel.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# usage: ./hack/test-wheel <wheel_path>
+#
+# Used in build-and-publish workflow to verify the wheel before publishing
+#
+
+set -eu
+
+export UV_PYTHON_DOWNLOADS=never
+
+wheel_path=$1
+test -f "$wheel_path"
+
+for V in 3.9 3.10 3.11 3.12 3.13; do
+    echo "::group::$(python${V} -VV)"
+    rm -rf .venv
+    uv venv -p ${V}
+    # shellcheck disable=SC1091
+    . .venv/bin/activate
+    uv pip install pytest "$wheel_path"
+    pytest integration_tests/
+    deactivate
+    echo "::endgroup::"
+done
