28e4b33a672560be1991c4a50adac1a6727688dd
cakemanny <goldingd89@gmail.com>
2026-09-09 18:10:09 +0200

    test: fix test for too many threads

--- tests/pymontrace/test_attacher_noroot.py
+++ tests/pymontrace/test_attacher_noroot.py
@@ -13,7 +13,10 @@ import subprocess
     ((1, ["wrong"], "print('hi')"), TypeError,),
     ((1, [1, "wrong"], "print('hi')"), TypeError,),
     ((1, [1, 2, 3, 4, 5, 6, 7, 8,
-          9, 10, 11, 12, 13, 14, 15, 16, 17], "print('hi')"), ValueError,),
+          9, 10, 11, 12, 13, 14, 15, 16,
+          17, 18, 19, 20, 21, 22, 23, 24,
+          25, 26, 27, 28, 29, 30, 31, 32,
+          33], "print('hi')"), ValueError,),
     ((1, [-5], "print('hi')"), OverflowError,),
 ])
 def test_exec_in_threads(args, exc_type):
